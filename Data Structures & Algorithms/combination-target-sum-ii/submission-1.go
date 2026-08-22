cf5be617080fb0473fc4db532c39874f1a43c293
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-21 19:20:24 -0700

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.go
@@ -0,0 +1,52 @@
+import "slices"
+
+func combinationSum2(nums []int, target int) [][]int {
+	slices.Sort(nums)
+    var res [][]int
+	var path []int
+	var dfs func(int, int)
+
+	dfs = func(start, target int) {
+		if target == 0 {
+			t := append([]int{}, path...)
+			res = append(res, t)
+			return
+		}
+
+		for i:=start; i<len(nums); i++ {
+			if i > start && nums[i] == nums[i-1] {
+				continue
+			}
+			if target - nums[i] < 0 {
+				return
+			}
+			path = append(path, nums[i])
+			dfs(i+1, target - nums[i])
+			path = path[:len(path)-1]
+		}
+	}
+
+	dfs(0, target)
+
+	return res
+}
+
+// func unique(path []int, res [][]int) bool {
+// 	for _, r := range res {
+// 		if len(path) != len(r) {
+// 			continue
+// 		}
+// 		match := true
+// 		for i, rv := range r {
+// 			if rv != path[i] {
+// 				match = false
+// 				break
+// 			}
+// 		}
+// 		if match {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
