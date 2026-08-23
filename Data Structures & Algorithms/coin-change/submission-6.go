811367500804537528c257d726cbe3ba45ce9fe1
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-23 16:47:00 -0700

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.go
@@ -0,0 +1,37 @@
+func coinChange(coins []int, amount int) int {
+	cache := make(map[int]int)
+	cache[0] = 0
+
+	var minCoinsAt_toMake func(int) int
+	minCoinsAt_toMake = func(amount int) int {
+		if val, ok := cache[amount]; ok {
+			return val
+		}
+
+		minCoinsForThisAmount := math.MaxInt
+
+		for _, coin := range coins {
+			diff := amount - coin
+
+			if diff < 0 {
+				continue
+			}
+
+			res := minCoinsAt_toMake(diff)
+			if res != -1 {
+				minCoinsForThisAmount = min(minCoinsForThisAmount, 1+res)
+			}
+		}
+
+		if minCoinsForThisAmount == math.MaxInt {
+			cache[amount] = -1
+		} else {
+			cache[amount] = minCoinsForThisAmount
+		}
+
+		return cache[amount]
+	}
+
+	// 6. Call the recursive function to kick off the process
+	return minCoinsAt_toMake(amount)
+}
