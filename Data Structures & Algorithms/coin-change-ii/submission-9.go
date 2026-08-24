f04f39fa92313237d95c9017b7df009a0da6598c
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-23 20:56:38 -0700

    Add: coin-change-ii - submission-9

---
+++ Data Structures & Algorithms/coin-change-ii/submission-9.go
@@ -0,0 +1,11 @@
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+
+	for _, c := range coins{
+		for a := c; a<amount+1; a++{
+			dp[a] += dp[a-c]
+		}
+	} 
+	return dp[amount]
+}
