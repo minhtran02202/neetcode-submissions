ee17ed9f9006b8bf6e144437241579a618ee003b
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-24 15:45:12 -0700

    Add: longest-palindromic-substring - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.go
@@ -0,0 +1,36 @@
+func longestPalindrome(s string) string {
+	memo := make(map[[2]int]string)
+
+	var find func(int, int) string
+	find = func(start, end int) string {
+		if start > end { return "" }
+		if start == end { return string(s[start]) }
+
+		state := [2]int{start, end}
+
+		if val, ok := memo[state]; ok {
+			return val
+		}
+
+		if s[start] == s[end] {
+			inner := find(start + 1, end - 1)
+			if len(inner) == end - start - 1 {
+				memo[state] = s[start : end+1]
+				return memo[state]
+			}
+		} 
+
+		l := find(start + 1, end)
+		r := find(start, end - 1)
+
+		res := r
+		if len(l) > len(r) {
+			res = l
+		}
+
+		memo[state] = res
+		return res
+	}
+
+	return find(0, len(s) - 1)
+}
