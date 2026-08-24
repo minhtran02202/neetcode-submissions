d902c6a79e717bf0c3d0517a873bb9203bbe7000
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-24 13:53:40 -0700

    Add: house-robber-ii - submission-16

---
+++ Data Structures & Algorithms/house-robber-ii/submission-16.go
@@ -0,0 +1,17 @@
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	return max(nums[0], max(handle(nums[:len(nums) - 1]), handle(nums[1:])))
+}
+
+func handle(nums []int) int {
+	house1, house2 := 0, 0
+	
+	for _, val := range nums {
+		house1, house2 = house2, max(house2, house1 + val)
+	}
+
+	return house2 
+}
