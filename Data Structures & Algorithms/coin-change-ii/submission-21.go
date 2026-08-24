37fa111c342fc64973421b1cbdafa8e3dd74addd
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-23 21:47:17 -0700

    Add: coin-change-ii - submission-21

---
+++ Data Structures & Algorithms/coin-change-ii/submission-21.go
@@ -0,0 +1,24 @@
+func change(amount int, coins []int) int {
+    cache := make([][]int, len(coins))
+
+	for i := range cache{
+		cache[i] = make([]int, amount+1)
+		for j := range cache[i] {
+			cache[i][j] = -1
+		}
+	}	
+
+	var dfs func (int, int) int
+	dfs = func(at, target int) int {
+		if target == 0 { return 1 }
+		if at >= len(coins) || target < 0 { return 0 }
+
+		if cache[at][target] != -1 { return cache[at][target] }
+
+		cache[at][target] = dfs(at+1, target) + dfs(at, target-coins[at])
+
+		return cache[at][target]
+	}
+
+	return dfs(0, amount)
+}
