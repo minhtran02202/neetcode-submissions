afbd58ead760150822482353fd2340cb3cf35016
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-25 11:14:55 -0700

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.go
@@ -0,0 +1,22 @@
+func uniquePaths(m int, n int) int {
+    grid := make([][]int, m)
+
+	for i := 0; i < len(grid); i++ {
+		grid[i] = make([]int, n)
+
+		// set first row and column to 1, base case
+		grid[i][0] = 1
+		for j := 0; j < n; j++{
+			grid[0][j] = 1
+		}
+
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			grid[i][j] = grid[i-1][j] + grid[i][j-1] 
+		}
+	}
+
+	return grid[m-1][n-1]
+}
