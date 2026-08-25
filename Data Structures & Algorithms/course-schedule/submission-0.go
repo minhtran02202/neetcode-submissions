8b370c5a5c9025217be8ae5b38a195ab0d2c7fd3
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-08-25 14:52:37 -0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.go
@@ -0,0 +1,41 @@
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	hash := make(map[int][]int)
+
+	for _, val := range prerequisites {
+		if len(hash[val[0]]) == 0 {
+			hash[val[0]] = []int{}
+		}
+		hash[val[0]] = append(hash[val[0]], val[1])
+	}
+
+	visited := make(map[int]bool)
+
+	var dfs func(i int) bool
+
+	dfs = func(i int) bool {
+		if visited[i] {
+			return false
+		}
+
+		if hash == nil || hash[i] == nil || len(hash[i]) == 0 {
+			return true
+		}
+
+		visited[i] = true
+		for _, val := range hash[i] {
+			if !dfs(val) {
+				return false
+			}
+		}
+
+		delete(hash, i)
+		visited[i] = false
+		return true
+	}
+
+	for i := 0; i < numCourses; i++ {
+		if !dfs(i) { return false }
+	}
+
+	return true
+}
