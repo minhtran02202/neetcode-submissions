695f427d7c861457077d38709dad6bcbc88e934e
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-09-14 13:47:06 -0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    h := make(map[int]int, len(nums))
+
+	for i, val := range nums {
+		if ans, ok := h[target - val]; ok {
+			return []int{ans, i}
+		}
+		h[val] = i
+	}
+	
+	return []int{}
+}
