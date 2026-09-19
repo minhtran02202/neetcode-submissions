30d51e2b25d16395fa5e00077e5b4acd9e71226f
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-09-19 12:19:49 -0700

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.go
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxDepth(root *TreeNode) int {
+    if root == nil { return 0 }
+	return max(maxDepth(root.Right), maxDepth(root.Left)) + 1
+}
