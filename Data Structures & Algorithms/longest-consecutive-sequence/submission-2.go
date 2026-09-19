ae94220a50d59ef89ae758fe286ea97d0f34e625
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-09-19 11:15:17 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.go
@@ -0,0 +1,17 @@
+import "slices"
+
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 { return 0 }
+
+	slices.Sort(nums)
+	local, res := 1, 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] { continue }
+		if nums[i] - nums[i - 1] > 1 { local = 1; continue }
+		local++
+		res = max(local, res)
+	}
+
+	return res
+}
