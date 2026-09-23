d5ac0ef30b8477334cc96648a7d0c9444f319ba7
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-09-23 15:08:37 -0700

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.go
@@ -0,0 +1,39 @@
+// distinct ints
+// find all unique combs of nums == target
+// same nums can be reused
+// target is positive int
+
+// backtracking
+// need: sorted array
+
+// base conditions
+// target == 0; found 1 path (not taking any nums)
+// target < 0; over calc -> cur path not valid -> return 0
+// if i == len nums
+
+// actions: take cur num or take next num 
+
+import "slices"
+
+func combinationSum(nums []int, target int) [][]int {
+	slices.Sort(nums)
+	
+	n := len(nums)
+	var res [][]int
+	var dfs func(int, int, []int)
+
+	dfs = func(i, diff int, path []int) {
+		if diff == 0 {
+			res = append(res, append([]int{}, path...))
+			return
+		} else if diff < 0 || i == n { return }
+
+		for j := i; j < n; j++ {
+			dfs(j, diff - nums[j], append(path, nums[j]))
+		}
+	}
+
+	dfs(0, target, []int{})
+
+	return res
+}
