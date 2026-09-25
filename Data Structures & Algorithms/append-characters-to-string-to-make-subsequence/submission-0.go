641ff0cf1b4676596423f7b265ead0c02fc3d17e
Minh Tran <50005387+minhtran02202@users.noreply.github.com>
2026-09-25 15:20:25 -0700

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.go
@@ -0,0 +1,14 @@
+// find i index of t that is last valid subsequent char
+// return len t - i (since once valid, i auto move to next char, no need to handle offset by 1)
+func appendCharacters(s string, t string) int {
+    ps, pt := 0, 0
+
+	for ps < len(s) && pt < len(t) {
+		if s[ps] == t[pt] {
+			pt++
+		}
+		ps++
+	}
+
+	return len(t) - (pt)
+}
